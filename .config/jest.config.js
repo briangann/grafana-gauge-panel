ce56255e086b630237b910b232d45ae335176768
Brian Gann <bkgann@gmail.com>
2022-12-24 19:36:31 -0500

    remove duplicate

--- .config/jest.config.js
+++ .config/jest.config.js
@@ -23,7 +23,6 @@ module.exports = {
   testMatch: [
     '<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}',
     '<rootDir>/src/**/*.{spec,test,jest}.{js,jsx,ts,tsx}',
-    '<rootDir>/src/**/*.{spec,test,jest}.{js,jsx,ts,tsx}',
   ],
   transform: {
     '^.+\\.(t|j)sx?$': [
