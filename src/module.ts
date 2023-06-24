17d0e594433e4415a36f0ca64d6ffdfb31f42733
Brian Gann <bkgann@gmail.com>
2023-06-23 22:17:31 -0400

    start migrating settings

--- src/module.ts
+++ src/module.ts
@@ -1,9 +1,80 @@
-import { D3GaugePanelCtrl } from './ctrl';
-import { loadPluginCss } from 'grafana/app/plugins/sdk';
+import { FieldConfigProperty, PanelPlugin } from '@grafana/data';
+import { GaugePanel } from './components/GaugePanel';
+import { GaugeOptions, OperatorOptions } from 'components/types';
+import { DataSuggestionsSupplier } from './components/suggestions';
+import { PanelMigrationHandler } from './migrations';
 
-loadPluginCss({
-  dark: 'plugins/briangann-gauge-panel/styles/dark.css',
-  light: 'plugins/briangann-gauge-panel/styles/light.css',
-});
+export const plugin = new PanelPlugin<GaugeOptions>(GaugePanel)
+  .setMigrationHandler(PanelMigrationHandler)
+  .useFieldConfig({
+    disableStandardOptions: [
+      FieldConfigProperty.Thresholds,
+      FieldConfigProperty.Color,
+      FieldConfigProperty.Decimals,
+      FieldConfigProperty.DisplayName,
+      FieldConfigProperty.Max,
+      FieldConfigProperty.Min,
+      FieldConfigProperty.Links,
+      FieldConfigProperty.NoValue,
+      FieldConfigProperty.Unit,
+    ],
+    standardOptions: {
+      [FieldConfigProperty.Mappings]: {},
+    },
+  })
+  .setPanelOptions((builder) => {
+    builder
+      // General Settings
 
-export { D3GaugePanelCtrl as PanelCtrl };
+      // unit to use
+      .addUnitPicker({
+        name: 'Unit',
+        path: 'unitFormat',
+        defaultValue: 'short',
+        category: ['General'],
+        description: 'The Unit Format displayed with the metric value',
+      })
+      // stat (operator)
+      .addSelect({
+        name: 'Stat',
+        path: 'operatorName',
+        description: 'Statistic to display',
+        category: ['General'],
+        defaultValue: OperatorOptions[0].value,
+        settings: {
+          options: OperatorOptions,
+        },
+      })
+      // decimals
+      .addNumberInput({
+        name: 'Decimals',
+        path: 'decimals',
+        description: 'Display specified number of decimals',
+        defaultValue: 2,
+        settings: {
+          min: 0,
+          integer: true,
+        },
+        category: ['General'],
+      })
+      // valueYOffset
+      .addNumberInput({
+        name: 'ValueYOffset',
+        path: 'valueYOffset',
+        description: 'Adjust the displayed value up or down the Y-Axis, use negative value to move up, positive for down',
+        defaultValue: 0,
+        settings: {
+          min: 0,
+          integer: true,
+        },
+        category: ['General'],
+      })
+
+      // Font Settings
+
+      // unitsLabelFontSize
+      // unitsFont
+      // labelFontSize
+      // tickFont
+  })
+  .setSuggestionsSupplier(new DataSuggestionsSupplier());
