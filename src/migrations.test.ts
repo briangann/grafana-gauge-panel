cdfb089910414fd11c1e8b80c8321c101d05392c
Brian Gann <bkgann@gmail.com>
2023-06-23 22:17:15 -0400

    add migrations

---
+++ src/migrations.test.ts
@@ -0,0 +1,31 @@
+import { PanelModel } from '@grafana/data';
+import { config } from "@grafana/runtime";
+
+import {
+  PanelMigrationHandler,
+  hasRobotoFont,
+} from './migrations';
+
+describe('D3Gauge -> D3GaugeV2 migrations', () => {
+  it('only migrates old d3gauge', () => {
+    const panel = {} as PanelModel;
+    const options = PanelMigrationHandler(panel);
+    expect(options).toEqual({});
+  });
+
+  it('checks if roboto is available to runtime', () => {
+    const versions = new Map<string, boolean>([
+      ["8.4.11", true],
+      ["8.5.21", true],
+      ["9.1.0", true],
+      ["9.2.0", true],
+      ["9.3.0", true],
+      ["9.4.0", false],
+      ["9.4.3", false],
+    ]);
+    for (let [key, value] of versions) {
+      config.buildInfo.version = key;
+      expect(hasRobotoFont()).toEqual(value);
+    }
+  });
+});
---
+++ src/migrations.ts
@@ -0,0 +1,75 @@
+import { PanelModel } from '@grafana/data';
+import { config } from "@grafana/runtime";
+import { satisfies, coerce } from "semver";
+
+import { GaugeOptions } from './components/types';
+
+interface AngularOptions {
+  decimals: number;
+  format: string;
+  operatorName: string;
+}
+
+/**
+ * This is called when the panel is imported or reloaded
+ */
+export const PanelMigrationHandler = (panel: PanelModel<GaugeOptions>): Partial<GaugeOptions> => {
+  //@ts-ignore
+  if (!panel.gauge) {
+    // not angular, just return the options if currently set
+    if (!panel.options) {
+      // This happens on the first load or when migrating from angular
+      return {} as any;
+    }
+    // have settings, return them unchanged
+    return panel.options;
+  }
+  //@ts-ignore
+  const newDefaults = migrateDefaults(panel.gauge);
+  let options = newDefaults;
+  //@ts-ignore
+  delete panel.gauge;
+  return options;
+};
+
+export const migrateDefaults = (angular: AngularOptions) => {
+  let options: GaugeOptions = {
+    panelHeight: undefined,
+    panelWidth: undefined,
+    panelId: 0,
+    decimals: 2,
+    unitFormat: '',
+    operatorName: 'avg',
+  };
+  return options;
+};
+
+/**
+ * This is called when the panel changes from another panel
+ *
+ * not currently used
+ */
+export const PanelChangedHandler = (
+  panel: PanelModel<Partial<GaugeOptions>> | any,
+  prevPluginId: string,
+  prevOptions: any
+) => {
+  // Changing from angular polystat panel
+  if (prevPluginId === 'd3gauge' && prevOptions.angular) {
+    console.log('detected old panel');
+    const oldOpts = prevOptions.angular;
+    console.log(JSON.stringify(oldOpts));
+  }
+  return {};
+};
+
+// Roboto font was removed Dec 1, 2022, and releases after that date should not attempt to use it
+export const hasRobotoFont = () => {
+  const version = coerce(config.buildInfo.version);
+  if (version !== null) {
+    if (satisfies(version, "<9.4.0")) {
+      return true;
+    }
+  }
+  return false;
+};
